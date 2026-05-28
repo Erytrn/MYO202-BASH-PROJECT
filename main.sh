610caeafabc849e22c95e370597633fd82035d2e
Erytrn <ahmeteray.turan@gmail.com>
2026-05-28 17:47:53 +0300

    işlemci, ram, anakart ve mac donanım bilgileri eklendi

--- main.sh
+++ main.sh
@@ -10,3 +10,23 @@ LOG_FİLE="report.log"
 İSO_TARİH=$(date -Iseconds)
 echo "Başlangıç Zamanı: $ISO_TARIH" > $LOG_FILE
 
+echo "" >> $LOG_FILE
+echo "Donanım bilgisi" >> $LOG_FILE
+
+echo "CPU" >> $LOG_FILE
+wmic cpu get name >> $LOG_FILE
+
+echo "RAM" >> $LOG_FILE
+wmic memorychip get capacity >> $LOG_FILE
+
+echo "ANAKART" >> $LOG_FILE
+wmic baseboard get product,Manufacturer >> $LOG_FILE
+
+echo "Disk Sistem UUID" >> $LOG_FILE
+wmic csproduct get uuid >> $LOG_FILE
+
+ECHO "MAC Adresi" >> $LOG_FILE
+getmac >> $LOG_FILE
+
+echo "Donanım bilgileri yüklendi"
+
