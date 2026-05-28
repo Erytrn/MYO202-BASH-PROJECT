c1179bb534018e149a8d9cbae73bfe29ff61a19f
Erytrn <ahmeteray.turan@gmail.com>
2026-05-28 17:30:46 +0300

    report.log dostayı oluşturdum iso tarihi ekledim

--- main.sh
+++ main.sh
@@ -6,3 +6,7 @@
 # Sertifika 2: https://credsverse.com/credentials/abcac5ea-b99d-4618-999f-c1e93f828b09
 # Sertifika 3: https://www.btkakademi.gov.tr/portal/certificate/validate?certificateId=qKrhe9a4xw
 
+LOG_FİLE="report.log"
+İSO_TARİH=$(date -Iseconds)
+echo "Başlangıç Zamanı: $ISO_TARIH" > $LOG_FILE
+
