e77ec3305cfeb938ec396fe78c6ef845e62edf89
Erytrn <ahmeteray.turan@gmail.com>
2026-05-28 17:13:13 +0300

    sertifika ve bilgilerimi yükledim

---
+++ main.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Eray TURAN
+# Öğrenci Numarası: 2420191037
+# Sertifika 1: https://www.btkakademi.gov.tr/portal/certificate/validate?certificateId=6mqF0P6YKP
+# Sertifika 2: https://credsverse.com/credentials/abcac5ea-b99d-4618-999f-c1e93f828b09
+# Sertifika 3: https://www.btkakademi.gov.tr/portal/certificate/validate?certificateId=qKrhe9a4xw
+
